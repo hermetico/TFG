8e8452b91a489b1adf9d5f2a305533893c8b6127
hermetico <yojmarinv@gmail.com>
2015-10-29 14:12:27 +0100

    añadida estructura del importador, y archivos pendientes de importar en su correspondiente carpeta. Importador hasta este momento muestra todas las nuevas imagenes, con su ruta separando user, año, mes...

--- annotation-tool/manage.py
+++ annotation-tool/manage.py
@@ -4,7 +4,7 @@ import sys
 
 reload(sys)
 sys.setdefaultencoding("utf-8")
-
+import os
 from app import create_app, db
 from app.models import User, Role, Picture, Label
 from flask.ext.script import Manager, Shell
@@ -23,12 +23,26 @@ manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
 
 
+## otra forma de definir los comandos para el manager
 @manager.command
 def test():
     """Run the unit tests."""
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
+
+@manager.command
+def importsuite():
+    from importtools import importer
+
+    IMPORT_FOLDER_NAME = "import bucket"
+    route = os.path.join(os.path.dirname(os.path.abspath(IMPORT_FOLDER_NAME)), IMPORT_FOLDER_NAME)
+    context = dict(app=app, db=db, User=User, Label=Label, Picture=Picture)
+    importer.load(route, context)
+
+
+
+
 
 
 
