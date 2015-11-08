b88e815bfe8fe9406ab0228ae8e7263e22a3c1dc
hermetico <yojmarinv@gmail.com>
2015-11-09 00:38:51 +0100

    server in production

--- annotation-tool/manage.py
+++ annotation-tool/manage.py
@@ -11,12 +11,12 @@ from flask.ext.script import Manager, Shell
 from flask.ext.migrate import Migrate, MigrateCommand, upgrade
 
 
-#if os.path.exists('.env'):
-#    print('Importing environment from .env...')
-#    for line in open('.env'):
-#        var = line.strip().split('=')
-#        if len(var) == 2:
-#            os.environ[var[0]] = var[1]
+if os.path.exists('.env'):
+    print('Importing environment from .env...')
+    for line in open('.env'):
+        var = line.strip().split('=')
+        if len(var) == 2:
+            os.environ[var[0]] = var[1]
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
